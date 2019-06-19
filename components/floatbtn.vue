a7cea3545dd1276160b4844d01a56f0e472ba17b
SKR5521 <48979021+SKR5521@users.noreply.github.com>
2019-06-19 20:26:52 +0530

    tooltip updated

--- components/floatbtn.vue
+++ components/floatbtn.vue
@@ -7,7 +7,25 @@
           <v-icon>close</v-icon>
         </v-btn>
       </template>
-      <v-btn fab dark small color="green" to="/gallery">
+      <v-tooltip left open-delay="100">        
+          <v-btn fab dark small color="green" to="/gallery" slot="activator">
+            <v-icon>photo</v-icon>
+          </v-btn>        
+        <span>Gallery</span>
+      </v-tooltip>
+      <v-tooltip left open-delay="100">        
+          <v-btn fab dark small color="indigo" to="/process" slot="activator">
+            <v-icon>games</v-icon>
+          </v-btn>        
+        <span>Process</span>
+      </v-tooltip>
+      <v-tooltip left open-delay="100">
+        <v-btn fab dark small color="red" to="/" slot="activator">
+          <v-icon>home</v-icon>
+        </v-btn>
+        <span>Home</span>
+      </v-tooltip>
+      <!-- <v-btn fab dark small color="green" to="/gallery">
         <v-icon >photo</v-icon>
       </v-btn>
       <v-btn fab dark small color="indigo" to="/process">
@@ -15,7 +33,7 @@
       </v-btn>
       <v-btn fab dark small color="red" to="/">
         <v-icon >home</v-icon>
-      </v-btn>
+      </v-btn>-->
     </v-speed-dial>
   </div>
 </template>
